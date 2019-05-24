bc4e4c9e7ed420baa56e8aff72470142613a931b
chenqiaojie <88155745@qq.com>
2019-05-24 14:33:47 +0800

    fix(*)

---
+++ src/ArrayUtil.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace phpType;
+
+/**
+ * 数组工具
+ * Class ArrayUtil
+ * @package App\Utils
+ */
+class ArrayUtil
+{
+    /**
+     * 把两个字段转换成 关系型一维数组
+     * @param $array
+     * @param string $key
+     * @param string $value
+     * @return array
+     * @author chenqiaojie 2018-12-21
+     */
+    public function arrayKeyValue($array, string $key, string $value)
+    {
+        $arr = [];
+        foreach ($array as $k => $v) {
+            $arr[$v[$key]] = $v[$value];
+        }
+        return $arr;
+    }
+
+    /**
+     * 把二维数组中一个值当做键名 转成新数组
+     * 需要数据唯一性的
+     * @param $array
+     * @param string $key
+     * @return array
+     * @author chenqiaojie 2018-12-21
+     */
+    public function keyArray($array, $key)
+    {
+        $arr = [];
+        foreach ($array as $k => $v) {
+            $arr[$v[$key]] = $v;
+        }
+        return $arr;
+    }
+
+    /**
+     * 把二维数组中一个值当做键名 转成新三维数组
+     * 不需要数据唯一性的
+     * @param $array
+     * @param $key
+     * @return array
+     * @author chenqiaojie 2019-01-11
+     */
+    public function arrayKeyArray($array, $key): array
+    {
+        $arr = [];
+        foreach ($array as $k => $v) {
+            $arr[$v[$key]][] = $v;
+        }
+        return $arr;
+    }
+
+
+    /**
+     * 二维数组根据某个字段排序
+     * @param array $array 要排序的数组
+     * @param string $keys 要排序的键字段
+     * @param int $sort 排序类型  SORT_ASC     SORT_DESC
+     * @return array 排序后的数组
+     */
+    public function twoArraySort($array, $keys, $sort = SORT_DESC): array
+    {
+        $keysValue = [];
+        foreach ($array as $k => $v) {
+            $keysValue[$k] = $v[$keys];
+        }
+        array_multisort($keysValue, $sort, $array);
+        return $array;
+    }
+}
---
+++ src/Entity.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace phpType;
+
+
+/**
+ * 实例操作
+ * Class Entity
+ * @package phpType
+ */
+class Entity
+{
+    protected $instance;
+
+    /**
+     * 填充字段
+     * @var array
+     */
+    public static $fillable;
+
+    /**
+     * 设置实例
+     * @param $instance
+     * @author chenqiaojie 2018-08-07
+     */
+    public function setInstance($instance)
+    {
+        $this->instance = $instance;
+    }
+
+    /**
+     * 将下划线命名转换为驼峰式命名
+     * @param $str
+     * @param bool $ucfirst
+     * @return mixed|string
+     * @author chenqiaojie 2018-05-14
+     */
+    public static function convertUnder($str, $ucfirst = true)
+    {
+        $str = ucwords(str_replace('_', ' ', $str));
+        $str = str_replace(' ', '', lcfirst($str));
+        return $ucfirst ? ucfirst($str) : $str;
+    }
+
+    /**
+     * 转换数组
+     * @return array
+     * @author chenqiaojie 2018-08-07
+     */
+    public function toArray()
+    {
+        $arr = get_object_vars($this->instance); //对象属性转数组
+        $arr = array_filter($arr, function ($v, $k) {
+            return !empty($v) || $v === 0;
+        }, ARRAY_FILTER_USE_BOTH); //过滤掉为空的数组
+        unset($arr['instance']);
+        return $arr;
+    }
+
+    /**
+     * 获取对象属性
+     * @return array
+     * @author chenqiaojie 2018-08-07
+     */
+    private function getClassAttr()
+    {
+        $arr = get_object_vars($this->instance); //对象属性转数组
+        unset($arr['instance']);
+        return $arr;
+    }
+
+    /**
+     * 加载获得实例
+     * @param bool $select 要获取查询字段
+     * @return object|static|null
+     * @author chenqiaojie 2018-05-16
+     */
+    public static function getInstance(bool $select = false)
+    {
+        $instance = new static();
+        $instance->setInstance($instance);
+        if ($select === true) {
+            $classAttr = $instance->getClassAttr();
+            foreach ($classAttr as $key => $value) {
+                $instance->$key = $key;
+            }
+        }
+
+        return $instance;
+    }
+
+    /**
+     * 绑定数据
+     * @param $data
+     * @return object|static|null
+     * @author chenqiaojie 2018-05-07
+     */
+    public static function bind($data)
+    {
+        $instance = new static();
+        $instance->setInstance($instance);
+        $classAttr = $instance->getClassAttr();
+        foreach ($classAttr as $key => $value) {
+            $func = 'set' . $instance::convertUnder($key);
+            if (isset($data->$key)) {
+                $instance->$func($data->$key);
+            }
+        }
+        return $instance;
+    }
+
+    /**
+     * 绑定表单数据
+     * @param array $data
+     * @return object|static|null
+     * @author chenqiaojie 2018-05-13
+     */
+    public static function form(array $data)
+    {
+        $instance = new static();
+        $instance->setInstance($instance);
+        $fillable = $instance->getClassAttr();
+        foreach ($fillable as $key => $value) {
+            $func = 'set' . self::convertUnder($key);
+            if (isset($data[$key])) {
+                $instance->$func($data[$key]);
+            }
+        }
+        return $instance;
+    }
+}
